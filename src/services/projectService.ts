ff2355f5ed3ceceec68279637cdd341b9c6c26ca
sk-pathak <sumitpathak2002@gmail.com>
2024-11-14 08:53:42 +0530

    merged logic for search/sort

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -2,9 +2,15 @@ import { AxiosError } from "axios";
 import { configApi } from "../api/configApi";
 import { ProjectResponse } from "../types/projectTypes.ts";
 
-export const getProjects = async (page: number, limit: number = 4): Promise<ProjectResponse> => {
+export const getProjects = async (searchTerm: string = '', sortBy: string = '', order: 'asc' | 'desc' = 'asc', page: number, limit: number = 4): Promise<ProjectResponse> => {
   try {
-    const { data } = await configApi.get<ProjectResponse>(`/api/projects/all?page=${page}&size=${limit}`);
+    const params = new URLSearchParams();
+    if(searchTerm) params.append('searchTerm', searchTerm);
+    if(sortBy) params.append('sortBy', sortBy);
+    if(order) params.append('order', order);
+    params.append('page', page.toString());
+    params.append('size', limit.toString());
+    const { data } = await configApi.get<ProjectResponse>(`/api/projects/all?${params.toString()}`);
     return data;
   } catch (error) {
     if (error instanceof AxiosError) {
@@ -29,31 +35,3 @@ export const createProject = async (formData: FormData): Promise<ProjectResponse
     throw new Error('Could not create project')
   }
 }
-
-export const getSearchedProjects = async (searchTerm: string, page: number, limit: number=4): Promise<ProjectResponse> => {
-  try {
-    const { data } = await configApi.get<ProjectResponse>(`/api/projects/all?searchTerm=${searchTerm}&page=${page}&size=${limit}`);
-    return data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.log(error.response?.data);
-      throw new Error(error.response?.data)
-    }
-    console.log(error);
-    throw new Error('Cannot fetch projects')
-  }
-};
-
-export const getSortedProjects = async (sortBy: string, page: number, limit: number=4): Promise<ProjectResponse> => {
-  try {
-    const { data } = await configApi.get<ProjectResponse>(`/api/projects/all?sortBy=${sortBy}&page=${page}&size=${limit}`);
-    return data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.log(error.response?.data);
-      throw new Error(error.response?.data)
-    }
-    console.log(error);
-    throw new Error('Cannot fetch projects')
-  }
-};
